de997abc2bc2073b8c14ba5dc352e903b3fc2226
fercarballo88 <fercarballo@live.com>
2023-09-13 22:34:44 -0300

    tercerdesafio mod2

---
+++ index.js
@@ -0,0 +1,10 @@
+//const http = require("http")
+
+import http from "http"
+
+const server = http.createServer()
+
+
+server.listen(8080,()=>{
+    console.log("Escuchandoooooo");
+})
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+const app = express()
+// no es necesario que se appa, puede ser cualquier nombre
+// generalmente se nombra la constante app
+
+app.get("/",(req,res)=>{
+    res.send("Hola Puto")
+})
+
+app.get("/otrapag",(req,res)=>{
+    res.send("Agarrame esta")
+})
+
+app.listen(8080,()=>{
+    console.log("escuchando app");
+})

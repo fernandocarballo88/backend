6c37de85ff7c9777b4ec4f64b1d757f23783a534
fercarballo88 <fercarballo@live.com>
2023-10-08 12:05:28 -0300

    quintoDesafio mod2

--- src/students.manager.js
+++ src/students.manager.js
@@ -3,22 +3,25 @@ import { studentsModel } from "./db/models/students.models";
 class StudentsManager {
 
     async findAll(){
-
+        const response = await studentsModel.find()
+        return response
     }
 
     async findById(id){
-
+        const response = await studentsModel.findById(id)
+        return response
     }
 
     async createOne(obj){
-
+        const response = await studentsModel.create(obj)
+        return response
     }
 
     async updateOne(id,obj){
-
+        const response = await studentsModel.updateOne({_id:id}, {$set: obj});
     }
 
     async deleteOne(id){
-        
+        const response = await studentsModel.findByIdAndDelete(id)
     }
 }

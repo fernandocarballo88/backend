ee6ee221e9b4bcf320ffdf67a2cbbb27bb7022af
fercarballo88 <fercarballo@live.com>
2023-11-18 15:51:33 -0300

    octavoDesafio mod7

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -7,11 +7,8 @@ app.get('/auth/google',
 passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/auth/google/callback', 
-passport.authenticate('google', { failureRedirect: '/login' }),
-function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-    }); 
+passport.authenticate('google', { successRedirect: "/profile"})
+); 
 
 
 export default router
